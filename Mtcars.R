494e0d270fbcf52dc3f1d462c3e6528807cf573d
thierryracaud <thierryracaud@yahoo.fr>
2014-09-25 19:04:48 +0200

    New version of R file

--- Mtcars.R
+++ Mtcars.R
@@ -1,12 +1,27 @@
+# Exploring one of R core data sets called Mtcars
 data(mtcars)
+
+# Checking on summary statistics
+# Min, Max, quartiles, Median, Mean
 summary(mtcars)
-plot(mtcars)
+
+# Checking boxplot but it does not make much sense here
 boxplot(mtcars)
+
+# Checking what the variables are
 names(mtcars)
+
+# Checking what the variables are
+?mtcars
+
+# Trying a few plots
 plot(mtcars$wt, mtcars$mpg)
 plot(mtcars$mpg, mtcars$wt)
 plot(mtcars$mpg, mtcars$wt)
 head(mtcars)
+
+# Renaming variables
 plot(mtcars$hp, xlab = "Gross Horse Power", mtcars$wt, ylab = "Weight (lb/1000)")
 
-?mtcars
+# Drawing an histogram for gross horse power
+hist(mtcars$hp, xlab = "Gross Horse Power")
