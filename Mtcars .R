bfd70b9bacbe2b9660bb70233b3f7a3b06b692c2
thierryracaud <thierryracaud@yahoo.fr>
2014-09-25 18:45:04 +0200

    Changed the name of variables

---
+++ Mtcars .R
@@ -0,0 +1,12 @@
+data(mtcars)
+summary(mtcars)
+plot(mtcars)
+boxplot(mtcars)
+names(mtcars)
+plot(mtcars$wt, mtcars$mpg)
+plot(mtcars$mpg, mtcars$wt)
+plot(mtcars$mpg, mtcars$wt)
+head(mtcars)
+plot(mtcars$hp, xlab = "Gross Horse Power", mtcars$wt, ylab = "Weight (lb/1000)")
+
+?mtcars
